59856aefef17f725e27953287bcf3eec5784ccd0
lyana <lyana-m@yandex.ru>
2021-02-23 14:54:27 +0500

    function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checker = true;
+  while (checker) {
+    checker = false;
+    for (let subArr of bracketsConfig) {
+      let concat = subArr[0] + subArr[1];
+      if (str.includes(concat)) {
+        let index = str.indexOf(concat);
+        str = str.slice(0, index) + str.slice(index + 2);
+        checker = true;
+      }
+    }
+  }
+  return str.length == 0;
 }
